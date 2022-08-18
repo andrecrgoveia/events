b44f13b376b23a72bdd41908959b2416d5decb29
andrecrgoveia <andrecrgoveia@gmail.com>
2022-08-18 19:55:14 -0300

    Procfile was created

--- project/settings.py
+++ project/settings.py
@@ -15,6 +15,13 @@ from pathlib import Path
 
 # Developer's imports
 import os
+import environ
+
+
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vpghznx*f2!pjl-!!v6w%0)a$d_j1^!z#i6ntjeg_@j6enq+l5'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
